5cee9452df6cde4a5623fb20c053cdb9fc904870
Jelenal1 <jabbruh10@gmail.com>
2024-10-13 16:12:02 +0200

    style: user feedback for empty fields

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,7 +10,8 @@ const List = async ({ initialList }: { initialList: LIST }) => {
   return (
     <div className="mx-2 flex h-full min-w-72 flex-col gap-4 overflow-y-scroll rounded-md bg-gray-600 p-4">
       <ReactiveHeader
-        className="mx-2"
+        className="mx-1 whitespace-pre-line rounded-md p-1 text-xl font-bold focus:outline-none focus:ring-2 focus:ring-blue-500"
+        emptyClassName="mx-1 rounded-md p-1 text-xl outline outline-dashed outline-1 focus:outline-none focus:ring-2 focus:ring-blue-500 opacity-50"
         headerText={initialList.title}
         handleUpdate={async (innerText) => {
           "use server";
--- src/components/ReactiveHeader.tsx
+++ src/components/ReactiveHeader.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useState } from "react";
+
 const ReactiveHeader = ({
   className,
+  emptyClassName,
   headerText,
   handleUpdate,
 }: {
-  className: string;
+  className?: string;
+  emptyClassName?: string;
   headerText: string;
   handleUpdate: (innerText: string) => void;
 }) => {
+  const [isEmpty, setIsEmpty] = useState(headerText === "");
   return (
     <div
-      className={className}
+      className={isEmpty ? emptyClassName : className}
       onInput={(e) => {
         handleUpdate(e.currentTarget.innerText);
+        e.currentTarget.innerText === "" ? setIsEmpty(true) : setIsEmpty(false);
       }}
       contentEditable={true}
       suppressContentEditableWarning={true}
